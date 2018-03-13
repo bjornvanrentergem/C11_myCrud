0b46ae5235a2243444386ca01f82b604f4dcd30c
bjornvanrentergem <bjorn.van.rentergem@student.howest.be>
2018-03-13 10:13:54 +0100

    Config Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,19 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCU7JkDREc3Mhlu19lzcpDs1YGqttOEvH8",
+  authDomain: "c11mycrud.firebaseapp.com",
+  databaseURL: "https://c11mycrud.firebaseio.com",
+  projectId: "c11mycrud",
+  storageBucket: "c11mycrud.appspot.com",
+  messagingSenderId: "414421831897"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
